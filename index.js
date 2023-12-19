0533e8c3cbb3b36c014f56e1afa1ba2cb9977a54
dana.best.programmer@gmail.com <dana.best.programmer@gmail.com>
2023-12-19 19:53:30 +0800

    update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ wss.on("connection", (ws, req) => {
           ws.send('initdone');
         } else {
           console.log("Sending [msout] message...");
-          ws.send('[msout] '+message);
+          //ws.send('[msout] '+message);
         }
       });
       spawns[uid] = child;
